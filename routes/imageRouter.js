98a697205004dd0a5338fe18622934030224f3ba
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2022-12-15 16:47:35 +0200

    Update imageRouter.js

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/public/images')
+        cb(null, 'public/images')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)

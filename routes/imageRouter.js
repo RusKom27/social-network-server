b199da2c7356ad565d5e3f91198e7bd365537f1e
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2022-12-15 16:26:45 +0200

    Update imageRouter.js

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(process.cwd(), 'public/images'))
+        cb(null, path.join(process.cwd(), 'tmp/images'))
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
@@ -17,23 +17,7 @@ const upload = multer({ storage: storage })
 
 router.post('/upload', async (req, res, next) => {
     res.json(path.join(process.cwd(), 'public/images'))
-    try {
-        User.findByIdAndUpdate(
-            req.headers.authorization,
-            {
-                images: {
-                    avatar_image: {
-                        big: req.file.filename,
-                        small: req.file.filename
-                    }
-                }
-            }).then(user => {
-            console.log(path.join(process.cwd(), 'images'))
-            res.json(user)
-        })
-    } catch (err) {
-        res.json({message: err.message})
-    }
+
 });
 
 module.exports = router

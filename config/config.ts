2cbf0e321c332edd82b2936e05888953c47705f4
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2023-02-12 18:47:55 +0200

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config"
 
-const MONGO_URL = process.env.LOCAL_MONGO_URL || ""
+const MONGO_URL = (process.env.DEVELOPE ? process.env.LOCAL_MONGO_URL : process.env.MONGO_URL) || ""
 const SERVER_PORT = process.env.SERVER_PORT || '3000'
 const CLIENT_URL = process.env.DEVELOPE ? process.env.LOCAL_CLIENT_URL : process.env.CLIENT_URL
 
